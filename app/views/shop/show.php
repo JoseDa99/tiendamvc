f402b7bc6c29bd2226e0f0bab0939e42726d89c8
JoseDa99 <37418351+JoseDa99@users.noreply.github.com>
2022-11-18 20:34:31 +0100

    Ejercicio 4 Completado

--- app/views/shop/show.php
+++ app/views/shop/show.php
@@ -1,9 +1,9 @@
-<?php include_once dirname(__DIR__) . ROOT . 'header.php'?>
+<?php include_once dirname(__DIR__) . ROOT . 'header.php' ?>
 <h2 class="text-center"><?= $data['subtitle'] ?></h2>
 <img src="<?= ROOT ?>img/<?= $data['data']->image ?>" class="rounded float-right" alt="">
 <h4>Precio:</h4>
 <p><?= number_format($data['data']->price, 2) ?>€</p>
-<?php if ($data['data']->type == 1): ?>
+<?php if ($data['data']->type == 1) : ?>
     <h4>Descripción:</h4>
     <?= html_entity_decode($data['data']->description) ?>
     <h4>A quien va dirigido:</h4>
@@ -12,7 +12,7 @@
     <p><?= $data['data']->objetives ?></p>
     <h4>¿Qué es necesario conocer?</h4>
     <p><?= $data['data']->necesites ?></p>
-<?php elseif ($data['data']->type == 2): ?>
+<?php elseif ($data['data']->type == 2) : ?>
     <h4>Autor:</h4>
     <p><?= $data['data']->author ?></p>
     <h4>Editorial:</h4>
@@ -22,9 +22,12 @@
     <h4>Resumen:</h4>
     <?= html_entity_decode($data['data']->description) ?>
 <?php endif; ?>
-<a href="<?= ROOT . (!empty($data['back']) ? $data['back'] : 'shop') ?>" class="btn btn-success">Volver al listado de productos</a>
+<?php if ($data['back'] == "books") {
+    $volver = "libros";
+} elseif ($data['back'] == "courses") {
+    $volver = "cursos";
+}
+?>
+<a href="<?= ROOT . (!empty($data['back']) ? $data['back'] : 'shop') ?>" class="btn btn-success">Volver al listado de <?= $volver ?? 'productos'  ?></a>
 <a href="<?= ROOT ?>cart/addproduct/<?= $data['data']->id ?>/<?= $data['user_id'] ?>" class="btn btn-primary">Comprar</a>
-<?php include_once dirname(__DIR__) . ROOT . 'footer.php'?>
-
-
-
+<?php include_once dirname(__DIR__) . ROOT . 'footer.php' ?>
